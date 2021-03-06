98e6a9dc4622597b1764ceb52b3930145e4535ad
Alex Whitt <alex.joseph.whitt@gmail.com>
2021-03-06 18:31:10 +0000

    New ZFS structure + systemd-boot

--- machine.nix
+++ machine.nix
@@ -2,54 +2,23 @@
 {
   boot = {
     zfs = {
-      enableUnstable = true;
+      extraPools = [ "rpool" "bpool" ];
       forceImportAll = false;
-      extraPools = [ "bpool" ];
     };
 
     supportedFilesystems = [ "zfs" ];
 
     tmpOnTmpfs = true;
 
-    # TODO: Right alt doesn't work with console TTY switching
-    loader = {
-      grub = {
-        enable = true;
-        version = 2;
-        zfsSupport = true;
-      };
-    };
+  };
+  
+  fileSystems."/" = {
+    fsType = "zfs";
+    device = "rpool/ROOT/CRYPT/nixos";
   };
 
   services.zfs = {
     autoSnapshot.enable = true;
     autoScrub.enable = true;
-  };
-
-  fileSystems = {
-    "/" = {
-      device = "rpool/ROOT/nixos";
-      fsType = "zfs";
-    };
-    "/etc" = {
-      device = "rpool/ROOT/nixos/etc";
-      fsType = "zfs";
-    };
-    "/var" = {
-      device = "rpool/ROOT/nixos/var";
-      fsType = "zfs";
-    };
-    "/var/tmp" = {
-      device = "rpool/ROOT/nixos/var/tmp";
-      fsType = "zfs";
-    };
-    "/var/cache" = {
-      device = "rpool/ROOT/nixos/var/cache";
-      fsType = "zfs";
-    };
-    "/boot" = {
-      device = "bpool/BOOT/nixos";
-      fsType = "zfs";
-    };
   };
 }
