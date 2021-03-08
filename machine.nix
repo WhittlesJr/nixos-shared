b23c811df98c11d6edbcc2602b544c0dba0deceb
Alex Whitt <alex.joseph.whitt@gmail.com>
2021-03-08 03:32:30 +0000

    Actually we do need legacy mountpoint mode. Grumble.

--- machine.nix
+++ machine.nix
@@ -1,20 +1,59 @@
 { lib, config, pkgs, ... }:
 {
   boot = {
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+      };
+      efi = {
+        canTouchEfiVariables = false;
+      };
+    };
+
     zfs = {
-      extraPools = [ "rpool" "bpool" ];
+      requestEncryptionCredentials = true;
       forceImportAll = false;
     };
 
     supportedFilesystems = [ "zfs" ];
 
     tmpOnTmpfs = true;
+  };
 
+  fileSystems."/boot" =
+  { device = "bpool/BOOT/nixos";
+    fsType = "zfs";
   };
   
-  fileSystems."/" = {
+  fileSystems."/" =
+  { device = "rpool/CRYPT/ROOT/nixos";
     fsType = "zfs";
-    device = "rpool/ROOT/CRYPT/nixos";
+  };
+
+  fileSystems."/home" =
+  { device = "rpool/CRYPT/home";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var" =
+  { device = "rpool/CRYPT/ROOT/nixos/var";
+    fsType = "zfs";
+  };
+
+  fileSystems."/nix" =
+  { device = "rpool/CRYPT/ROOT/nixos/nix";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var/tmp" =
+  { device = "rpool/CRYPT/ROOT/nixos/var/tmp";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var/cache" =
+  { device = "rpool/CRYPT/ROOT/nixos/var/cache";
+    fsType = "zfs";
   };
 
   services.zfs = {
