a10101113c9633844acc58ae66c9bf0b9db532e9
Alex Whitt <alex.joseph.whitt@gmail.com>
2022-04-22 17:09:16 -0400

    Refactor ZFS to separate module

--- machine.nix
+++ machine.nix
@@ -13,54 +13,14 @@
 
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+      };
       efi = {
         canTouchEfiVariables = false;
       };
     };
-
-    zfs = {
-      requestEncryptionCredentials = true;
-      forceImportAll = false;
-    };
-
-    supportedFilesystems = [ "zfs" ];
-
     tmpOnTmpfs = true;
-  };
-
-  fileSystems."/" =
-  { device = "rpool/crypt/nixos";
-    fsType = "zfs";
-  };
-
-  fileSystems."/home" =
-  { device = "rpool/crypt/home";
-    fsType = "zfs";
-  };
-
-  fileSystems."/var" =
-  { device = "rpool/crypt/nixos/var";
-    fsType = "zfs";
-  };
-
-  fileSystems."/nix" =
-  { device = "rpool/crypt/nixos/nix";
-    fsType = "zfs";
-  };
-
-  fileSystems."/var/tmp" =
-  { device = "rpool/crypt/nixos/var/tmp";
-    fsType = "zfs";
-  };
-
-  fileSystems."/var/cache" =
-  { device = "rpool/crypt/nixos/var/cache";
-    fsType = "zfs";
-  };
-
-  services.zfs = {
-    autoSnapshot.enable = true;
-    autoScrub.enable = true;
   };
 }
---
+++ zfs.nix
@@ -0,0 +1,71 @@
+{ lib, config, pkgs, ... }:
+{
+  boot = {
+    zfs = {
+      requestEncryptionCredentials = true;
+      forceImportAll = false;
+      zed = {
+        enableMail = true;
+      };
+    };
+    supportedFilesystems = [ "zfs" ];
+  };
+
+  services.zfs = {
+    autoSnapshot.enable = true;
+    autoScrub.enable = true;
+  };
+
+  fileSystems."/" = {
+    device = "rpool/crypt/nixos";
+    fsType = "zfs";
+  };
+
+  fileSystems."/home" = {
+    device = "rpool/crypt/home";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var" = {
+    device = "rpool/crypt/nixos/var";
+    fsType = "zfs";
+  };
+
+  fileSystems."/nix" = {
+    device = "rpool/crypt/nixos/nix";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var/tmp" = {
+    device = "rpool/crypt/nixos/var/tmp";
+    fsType = "zfs";
+  };
+
+  fileSystems."/var/cache" = {
+    device = "rpool/crypt/nixos/var/cache";
+    fsType = "zfs";
+  };
+
+  programs.msmtp = {
+    enable = true;
+    setSendmail = true;
+    defaults = {
+      aliases = "/etc/aliases";
+      port = 465;
+      tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
+      tls = "on";
+      auth = "login";
+      tls_starttls = "off";
+    };
+    accounts = {
+      default = {
+        host = "smtp.gmail.com";
+        port = 587;
+        passwordeval = "gpg --quiet --for-your-eyes-only --no-tty --decrypt ~/.mail/.msmtp-credentials.gpg";
+        from = "alex.joseph.whitt@gmail.com";
+        user = "alex.joseph.whitt";
+      };
+    };
+  };
+
+}
