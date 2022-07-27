4e8278bb996aadcbbd41ed8619767c09e922b115
Alex Whitt <alex.joseph.whitt@gmail.com>
2022-07-26 22:38:25 -0400

    Add prototn-ge

--- gaming.nix
+++ gaming.nix
@@ -1,13 +1,33 @@
 { config, pkgs, ... }:
+let
+  steam-proton-ge =
+    pkgs.stdenv.mkDerivation rec {
+      pname = "proton-ge-custom";
+      version = "GE-Proton7-27";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
+        sha256 = "07p6dskl1rp39kkbjw4s36rs2dhmr15dj7h17ly2ckm2lsk2miss";
+      };
+
+      buildCommand = ''
+        mkdir -p $out
+        tar -C $out --strip=1 -x -f $src
+      '';
+    };
+in
 {
-  environment.systemPackages = with pkgs; [
-    discord
-    xboxdrv   # For Nintendo Switch pro controllers
-    evtest
+  environment.systemPackages = [
+    pkgs.discord
+    pkgs.xboxdrv   # For Nintendo Switch pro controllers
+    pkgs.evtest
+    steam-proton-ge
   ];
 
   programs.steam.enable = true;
   services.joycond.enable = true;
 
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${steam-proton-ge}";
+  };
 }
-
