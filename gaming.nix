445a52c4463456ac1f542be75a4700b5414b116b
Alex Whitt <alex.joseph.whitt@gmail.com>
2022-09-11 07:54:04 -0400

    Back to udev for pro controllers

--- gaming.nix
+++ gaming.nix
@@ -104,7 +104,19 @@ in
   ];
 
   programs.steam.enable = true;
-  services.joycond.enable = true;
+  #services.joycond.enable = true;
+
+  services.udev.extraRules = ''
+    # Nintendo Switch Pro Controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="2009", MODE="0666", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:057E:2009.*", MODE="0666"
+
+    # Nintendo Switch Pro Controller over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*057E:2009*", MODE="0666"
+
+    # Permissions for /dev/uinput
+    KERNEL=="uinput", MODE="0666", GROUP="users", OPTIONS+="static_node=uinput"
+  '';
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${steam-proton-ge}";
